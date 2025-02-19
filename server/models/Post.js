9c394a6ce4870cadd57a462715c7a6bb0b13c7da
Victor-Bonnelly <victor@ilevel.fr>
2025-02-19 16:55:05 +0100

    Probleme connexion 404

--- server/models/Post.js
+++ server/models/Post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({
   },
   author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Référence au modèle User
+    ref: 'User', 
     required: true,
   },
   createdAt: {
@@ -20,9 +20,9 @@ const postSchema = new mongoose.Schema({
   },
 });
 
-// Vous pouvez ajouter des méthodes au schéma si nécessaire
+
 postSchema.methods.getSummary = function() {
-  return this.content.substring(0, 100) + '...'; // Exemple de méthode pour obtenir un résumé du contenu
+  return this.content.substring(0, 100) + '...'; 
 };
 
 const Post = mongoose.model('Post', postSchema);
--- server/routes/post-routes.js
+++ server/routes/post-routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import { createPost, getAllPosts } from '../controllers/post-controller.js';
 import { recupererTousPosts } from '../db/repository/post-repository.js';
+import Post from '../db/models/Post.js';
 
 const router = express.Router();
-
 
 router.post('/post', createPost);
 
 router.get('/posts', async (req, res) => {
     try {
-        const posts = await recupererTousPosts();
+        const posts = await Post.find();
         res.status(200).json(posts);
     } catch (error) {
-        res.status(500).send({ message: 'Erreur lors de la récupération des posts' });
+        res.status(400).json({ error: error.message });
     }
 });
 
@@ -23,6 +23,17 @@ router.get('/api/posts', async (req, res) => {
     } catch (error) {
         console.error('Erreur lors de la récupération des posts:', error);
         res.status(500).json({ message: 'Erreur lors de la récupération des posts' });
+    }
+});
+
+router.post('/posts', async (req, res) => {
+    const { title, content } = req.body;
+    try {
+        const newPost = new Post({ title, content });
+        await newPost.save();
+        res.status(201).json({ message: 'Post created successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 });
 
